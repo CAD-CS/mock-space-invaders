1df14967ed10228d167ee75d211363db86d195a0
CAD-CS <parsa.hara117@gmail.com>
2025-09-20 14:54:32 -0700

    Created initial loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,19 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
 int main()
 {
-  return 0;
+    sf::RenderWindow window(sf::VideoMode(640, 480), "SFML Window");
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        
+    }
+    return 0;
 }
